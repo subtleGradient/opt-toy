79919279e182e02d0ad9b4bfb4c5304828dcddf0
Thomas Aylott <oblivious@subtlegradient.com>
2020-03-15 02:31:05 -0500

    cleanup

--- src/OPCodeInput.tsx
+++ src/OPCodeInput.tsx
@@ -1,36 +1,47 @@
-import * as React from "react";
-import { OPT512 } from "./OPT512";
-import { parseCoinText, cleanCoinText } from "./Coin";
+import * as React from "react"
+import { OPT512 } from "./OPT512"
+import { parseCoinText, cleanCoinText, OPT512Maybe } from "./Coin"
 
-export function OPCodeInput({ type, onParsed }) {
-  const opType = new OPT512(type);
-  let inputTypeText = opType.OP512;
-  if (opType.isEmpty) inputTypeText = "";
-  let [value, setValue] = React.useState(inputTypeText);
-  let [isEditing, setIsEditing] = React.useState(false);
+export function OPCodeInput({
+  coins,
+  onParsed,
+  style = null,
+  ...props
+}: React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+> & {
+  coins: OPT512Maybe
+  onParsed: (type: OPT512Maybe) => void
+  style?: React.CSSProperties
+}) {
+  const opType = new OPT512(coins)
+  let inputTypeText = opType.OP512
+  if (opType.isEmpty) inputTypeText = ""
+  let [value, setValue] = React.useState(inputTypeText)
+  let [isEditing, setIsEditing] = React.useState(false)
   // if (!isEditing) value = inputTypeText
   // if (type == null) type = value
   // else value = inputTypeText
-  function handleChange(event) {
-    const newValue = cleanCoinText(event.currentTarget.value);
-    setValue(newValue);
-    onParsed(parseCoinText(newValue));
+  function handleChange(event: { currentTarget: { value: string } }) {
+    const newValue = cleanCoinText(event.currentTarget.value)
+    setValue(newValue)
+    onParsed(parseCoinText(newValue))
   }
   return (
-    <React.Fragment>
-      <input
-        style={{ opacity: isEditing ? 1 : 0.8 }}
-        placeholder={inputTypeText}
-        onChange={handleChange}
-        value={isEditing ? value : inputTypeText}
-        onFocus={e => {
-          setValue(cleanCoinText(inputTypeText));
-          setIsEditing(true);
-        }}
-        onBlur={e => {
-          setIsEditing(false);
-        }}
-      />
-    </React.Fragment>
-  );
+    <input
+      {...props}
+      style={{ opacity: isEditing ? 1 : 0.8, ...style }}
+      placeholder={inputTypeText}
+      onChange={handleChange}
+      value={isEditing ? value : inputTypeText}
+      onFocus={e => {
+        setValue(cleanCoinText(inputTypeText))
+        setIsEditing(true)
+      }}
+      onBlur={e => {
+        setIsEditing(false)
+      }}
+    />
+  )
 }
