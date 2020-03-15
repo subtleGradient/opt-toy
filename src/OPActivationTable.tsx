f8be99e4ba1c76a70f007bb9459617cc4e4a9651
Thomas Aylott <oblivious@subtlegradient.com>
2020-03-15 02:30:50 -0500

    NEW Activations Table view

---
+++ src/OPActivationTable.tsx
@@ -0,0 +1,102 @@
+import * as React from "react"
+import { OPT512 } from "./OPT512"
+import "./OPActivationTable.css"
+import { betweenX } from "./between"
+
+export default function OPActivationTable({ op512 }: { op512: OPT512 }) {
+  const showSex = op512.fmDe !== "?" || op512.fmS !== "?"
+  return (
+    <div
+      className="OPActivationTable"
+      style={{
+        padding: betweenX(0, 14),
+        paddingBottom: 0,
+        fontSize: betweenX(8, 14),
+      }}
+    >
+      <table className="layout">
+        <tbody>
+          <tr>
+            <td style={{ width: "66.6%" }}>
+              <table className="functions">
+                <thead>
+                  <tr>
+                    <th>Active</th>
+                    {showSex && <th title="modality">Sex</th>}
+                    <th>Function</th>
+                    <th>Savior</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {op512.functions.map(
+                    ({
+                      code,
+                      activation1or2,
+                      activation,
+                      saviorCode,
+                      opFn,
+                    }) => (
+                      <tr key={code}>
+                        <td title={activation + 1}>{activation1or2}</td>
+                        {showSex && <td
+                          title={{ f: "feminine", m: "masculine" }[opFn?.sex]}
+                        >
+                          {opFn?.sex}
+                        </td>}
+                        <td>
+                          {code}
+                          {opFn?.focus}
+                        </td>
+                        <td>{saviorCode}</td>
+                      </tr>
+                    ),
+                  )}
+                </tbody>
+              </table>
+            </td>
+            <td style={{ width: "33.3%" }}>
+              <table className="animals">
+                <thead>
+                  <tr>
+                    <th>Savior</th>
+                    <th>Animal</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {op512.animalStack.map(({ code, index }) => (
+                    <tr key={code}>
+                      <td
+                        title={
+                          {
+                            0: "First Animal",
+                            1: "Secondary Savior Animal",
+                            2: `Activated Demon "Hobby" Animal`,
+                            3: "Last Animal",
+                          }[index]
+                        }
+                      >
+                        {{ 0: "S1", 1: "S2", 2: "A", 3: "-" }[index]}
+                      </td>
+                      <td
+                        title={
+                          {
+                            P: "Play",
+                            B: "Blast",
+                            C: "Consume",
+                            S: "Sleep",
+                          }[code]
+                        }
+                      >
+                        {code}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
