e14a6402c30e043f8df2e00fe1177fdba60ab443
Thomas Aylott <oblivious@subtlegradient.com>
2020-03-28 13:10:14 -0500

    types for styled-jsx

---
+++ src/macro.d.ts
@@ -0,0 +1,11 @@
+declare module "styled-jsx/macro" {
+  declare function css(chunks: TemplateStringsArray, ...args: any[]): string
+  declare namespace css {
+    function global(chunks: TemplateStringsArray, ...args: any[]): string
+    function resolve(
+      chunks: TemplateStringsArray,
+      ...args: any[]
+    ): { className: string; styles: string }
+  }
+  export = css
+}
