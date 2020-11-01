01d03892c356090a3a05282c1fe1f13cbaa390ac
Thomas Aylott <oblivious@subtlegradient.com>
2020-11-01 13:29:08 -0500

    ++

--- components/opt-toy/euclideanDistance.ts
+++ components/opt-toy/euclideanDistance.ts
@@ -2,6 +2,9 @@ export const euclideanDistance = (aaa: number[], bbb: number[]) => {
   return Math.sqrt(euclideanDistanceSquared(aaa, bbb))
 }
 
+/**
+ * The output of Jarvis-Patrick and K-Means clustering is not affected if Euclidean distance is replaced with Euclidean squared. However, the output of hierarchical clustering is likely to change.
+ */
 export const euclideanDistanceSquared = (aaa: number[], bbb: number[]) => {
   let distance = 0
   for (let index = 0; index < aaa.length; index++) {
