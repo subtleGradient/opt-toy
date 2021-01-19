f53ce4f9e760caded8d33c5c72485baf07428a18
Thomas Aylott <oblivious@subtlegradient.com>
2021-01-19 09:32:39 -0500

    sorterize

--- components/opt-toy/sortBy.tsx
+++ components/opt-toy/sortBy.tsx
@@ -1,3 +1,8 @@
-export const sortBy = (a: string | number, b: string | number) => a > b ? 1 : b > a ? -1 : 0;
+export const sortBy = (a: string | number, b: string | number) =>
+  a > b ? 1 : b > a ? -1 : 0
 
-export const sortByRandom = () => sortBy(Math.random(), Math.random())
+export const sorterize = <T extends any>(
+  getSortValue: (thing: T) => number | string,
+) => (a: T, b: T) => sortBy(getSortValue(a), getSortValue(b))
+
+export const sortByRandom = sorterize(Math.random)
