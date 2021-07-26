5aeb1b89de30f597551b982c0eae0cbfc597a914
Tom Aylott <oblivious@subtlegradient.com>
2021-07-26 12:01:45 -0400

    +tests

---
+++ components/opt-toy/Coin.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect, test } from "@jest/globals"
+import { COINS } from "./Coin"
+
+describe("COINS", () => {
+  it("exists", () => {
+    expect(COINS).toBeDefined()
+  })
+})
